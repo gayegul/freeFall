a4fb35ce4eef5016567c316f1839994fe09ef3c0
Gaye Bulut <gayegul@gmail.com>
2015-11-02 10:47:26 -0800

    refactor, add play/pausebuttons

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
-var time = 0.01;
 var bottomLimit = 355;
-var upperLimit = 9;
+var upperLimit = 7;
+var marsGravity = 3.711;
+var neptunGravity = 11.15;
+var uranusGravity = 8.69;
+var timeIncrement = 0.5;
+var balls = [];
 
 function Ball(gravity, posX, posY) {
   this.gravity = gravity;
@@ -10,44 +14,48 @@ function Ball(gravity, posX, posY) {
   this.element = $('<div class="ball">');
   this.element.appendTo($('#container'));
   this.element.offset({top: this.posY, left: this.posX});
+  this.velocity = 0;
+  balls.push(this);
 }
 
-Ball.prototype.stepOne = function(number) {
-  var inlimit = true;
-  if(number === -1 && (this.posY >= bottomLimit || this.posY > upperLimit)) {
-    inlimit = false;
-  }
-  else if(number === 1 && (this.posY <= upperLimit || this.posY < bottomLimit)) {
-    inlimit = false;
-  }
-  if(!inlimit) {
-    time += 0.01;
-    this.distance = 0.5 * this.gravity * time * time;
-    this.posY += (number * this.distance);
-    this.element.offset({top: this.posY, left: this.posX});
-  }
-  else {
-    clearInterval(this.interval);
-    this.start(number);
+Ball.prototype.updatePosition = function(time) {
+  var distanceTravelled = this.velocity * time + 0.5 * this.gravity * time * time;
+  this.posY += distanceTravelled;
+  this.element.offset({top: this.posY, left: this.posX});
+
+  this.velocity += this.gravity * time;
+
+  if(this.posY > bottomLimit || this.posY < upperLimit) {
+    this.velocity *= -1;
   }
 };
 
-Ball.prototype.start = function(number) {
-  time = 0.01;
-  this.interval = setInterval(this.stepOne.bind(this, number * -1), 1000 * time);
-};
+function animate() {
+  interval = setInterval(function() {
+    for(var i = 0; i < balls.length; i++) {
+      balls[i].updatePosition(timeIncrement);
+    }
+  }, 1000/26);
+}
 
+var x = 50;
+var y = 8;
 var earthBall = new Ball(9.8, 50, 8);
-// earthBall.start(-1); //uncomment it for constant fall and jump
-$('.planetList').addClass('hidden');
 
-$('#planets').hover(
-  function() {
-  $('.planetList').removeClass('hidden');
-}, function() {
-  $('.planetList').addClass('hidden');
+$('#pause').hide();
+$('#pause').on('click', function() {
+  $(this).hide();
+  clearInterval(interval);
+  $('#play').show();
+});
+
+$('#play').on('click', function() {
+  $(this).hide();
+  animate();
+  $('#pause').show();
 });
 
 //add wind, add friction options
 //activate dropdown menu for other planets
 //balls for other planets
+//add a master start & stop button
